91fcf4d31eeed96bb2e1d152a13c7fca04052166
Dennismwendwa <dennismusemb2@.com>
2023-02-16 12:31:32 +0300

    betty is something else though she makes the code readeble

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
 int main()
 {
-    char Mystr[] = "Programming is like building a multilingual puzzle";
-    
+    char Mystr[] = "\"Programming is like building a multilingual puzzle";
     puts(Mystr);
     
     return (0);
