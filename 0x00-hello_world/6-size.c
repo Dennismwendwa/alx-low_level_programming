c3b1d9d4719e9ca9ddd51b1323d6b394991c3aa2
Dennismwendwa <dennismusemb2@.com>
2023-02-16 13:40:09 +0300

    size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * Entry - main Entry point
+  *
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+
+char letters;
+int myNum;
+long longNum;
+double doubleNum;
+float money;
+
+printf("Size of the char is %d\n", (unsigned)sizeof(letters));
+printf("Size of the char is %d\n", (unsigned)sizeof(myNum));
+printf("Size of the char is %d\n", (unsigned)sizeof(longNum));
+printf("Size of the char is %d\n", (unsigned)sizeof(doubleNum));
+printf("Size of the char is %d\n", (unsigned)sizeof(money));
+
+return (0);
+}
