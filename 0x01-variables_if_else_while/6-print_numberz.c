e6306e9cbf437a25ef9af3da9054c080b6831cf7
Dennismwendwa <dennismusemb2@.com>
2023-02-17 13:51:20 +0300

    writting numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return:- Always 0 (success)
+  */
+
+int main(void)
+{
+int num = 0;
+
+for (; num < 10; num++)
+	putchar((num % 10) + '0');
+putchar('\n');
+
+return (0);
+}
