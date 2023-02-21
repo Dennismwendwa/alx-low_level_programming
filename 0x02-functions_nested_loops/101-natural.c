8117f1c6dcb2f628a559f10c841f5ba126673206
Dennismwendwa <dennismusemb2@.com>
2023-02-21 20:50:16 +0300

    added all numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - entry point.
+  * Return:- alway 0 (success)
+  */
+
+int main(void)
+{
+	int j = 0;
+	int sum = 0;
+
+	while (j < 1024)
+	{
+		if ((j % 3) == 0 || (j % 5) == 0)
+		{
+			sum = sum + j;
+
+		}
+		j++;
+
+	}
+
+	printf("%d", sum);
+
+	return (0);
+
+}
