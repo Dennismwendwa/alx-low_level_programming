ff4808041727b715e174113861ad858dfa0afc87
Dennismwendwa <dennismusemb2@.com>
2023-02-21 22:47:51 +0300

    updated last file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,7 +14,6 @@ int main(void)
 	{
 		sum = fib1 + fib2;
 		printf("%lu, ", sum);
-
 		fib1 = fib2;
 		fib2 = sum;
 	}
@@ -22,7 +21,6 @@ int main(void)
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
-
 	for (i = 93; i < 99; i++)
 	{
 		half1 = fib1_half1 + fib2_half1;
@@ -33,12 +31,10 @@ int main(void)
 			half2 %= 10000000000;
 		}
 		printf("%lu%lu", half1, half2);
-
 		if (i != 98)
 		{
 			printf(", ");
 		}
-
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
