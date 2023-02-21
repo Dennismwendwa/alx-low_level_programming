db925cba2ff4bf6030ff30278af2784593f30231
Dennismwendwa <dennismusemb2@.com>
2023-02-21 17:02:57 +0300

    ablote values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * _abs - function that computes absolute values of integer.
+  *
+  * @ab: single number input.
+  *
+  * Return:- always 0 (success)
+  */
+
+int _abs(int ab)
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+
+}
