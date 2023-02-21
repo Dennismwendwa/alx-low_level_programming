47453e1acbec060546e2a7148c35d30d7f38fc5e
Dennismwendwa <dennismusemb2@.com>
2023-02-21 19:00:12 +0300

    adding numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * add - function that adds two numbers.
+  * @x: first number input.
+  * @y: second number input.
+  *
+  * Return:- always 0 (success0
+  */
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+
+	return (sum);
+
+}
