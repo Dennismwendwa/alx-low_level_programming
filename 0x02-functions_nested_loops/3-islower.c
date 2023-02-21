3f45517a376d182b861176abdecd7d5b230d7b11
Dennismwendwa <dennismusemb2@.com>
2023-02-21 14:47:04 +0300

    returned lower case c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * 3-islower.c - checks for lowercase letters.
+  *
+  * Return:- Always 0 (succuss)
+  */
+
+int _islower(int c)
+{
+	int ch;
+
+	if (ch >= 'A' && ch <= 'Z')
+	{
+		return 0;
+
+
+	}
+	else if (ch >= 'a' && ch <= 'z')
+	{
+		return 1;
+
+
+	}
+
+}
