3cc0b78f3f7fd815bad682b17d7fdbd5e3c52306
Dennismwendwa <dennismusemb2@.com>
2023-02-23 11:35:59 +0300

    printing numbers without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - prints numbers from 0 upto 9.
+  * Return:- Always 0 (success).
+  */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+
+		}
+		else
+		{
+			_putchar(i + '0');
+
+
+		}
+
+
+
+	}
+	_putchar('\n');
+
+
+}
