f632f0f7d206b8f9455e1ac2e19c2e58025bd4af
Dennismwendwa <dennismusemb2@.com>
2023-02-23 11:21:22 +0300

    printing numbers

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+  * main - entry point
+  *
+  * Return:- Always 0 (success)
+  */
+
+int _putchar(char c)
+{
+
+
+	return (write(1, &c, 1));
+
+
+}
