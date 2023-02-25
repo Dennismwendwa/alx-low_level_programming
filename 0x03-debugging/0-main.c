c719017f6460186a18deb33e07d4b177b6d844f4
Dennismwendwa <dennismusemb2@.com>
2023-02-25 15:43:18 +0300

    updated file again

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
   * main - tests functions that printd if number is positive
   * Return:- Always 0 (success)
   */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
-	return (0);
 
+	return (0);
 }
