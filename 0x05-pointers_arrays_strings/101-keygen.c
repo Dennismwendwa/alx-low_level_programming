470a5477fdc658b8a98124ea4932776381161aae
Dennismwendwa <dennismusemb2@.com>
2023-02-27 18:50:22 +0300

    updated password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,19 +10,80 @@
 
 int main(void)
 {
-	int myrand;
-	int count;
-	int total = 2772;
+	char password[84];
+	int index;
+	int total;
+	int diff1;
+	int diff2;
 
-	srand(time(NULL));
-		for (count = 0; total > 122; count++)
+
+	srand(time(0));
+
+	while (total < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		total += password[index++];
+
+	}
+
+	password[index] = '\0';
+
+	if (total != 2772)
+	{
+		diff1 = (total - 2772) / 2;
+		diff2 = (total - 2772) / 2;
+
+		if ((total - 2772) % 2 != 0)
+			diff1++;
+
+		for (index = 0; password[index]; index++)
 		{
-			myrand = (rand() % 125) + 1;
-			printf("%dc", myrand);
-			total -= myrand;
+			if (password[index] >= (33 + diff1))
+			{
+				password[index] -= diff1;
+				break;
+
+			}
 
 		}
-	printf("%c", total);
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff2))
+			{
+				password[index] -= diff2;
+				break;
+			}
+
+		}
+
+
+	}
+	printf("%s", password);
 
 	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
