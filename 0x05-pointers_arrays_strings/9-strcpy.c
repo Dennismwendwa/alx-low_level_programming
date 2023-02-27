75dfb7665e687a3ed72403e7260398c57f49293d
Dennismwendwa <dennismusemb2@.com>
2023-02-27 16:47:44 +0300

    function that copy strngs

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _strcpy - Function that copy.
+  * @dest:- single user input.
+  * @src:- single user input.
+  * Return:- always 0 (success).
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+
+	}
+	return (dest);
+
+}
