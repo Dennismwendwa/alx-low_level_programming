604994132d931bfbc18d5c10791ee8ad46c1075a
Dennismwendwa <dennismusemb2@.com>
2023-02-27 16:33:24 +0300

    printing the values of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_array - Function that prints arrays.
+  * @a:- single user input.
+  * @n:- single user input.
+  */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+
+	}
+
+	if (i == (n - 1))
+	{
+		printf("%d", a[n-1]);
+	}
+
+	printf("\n");
+
+}
