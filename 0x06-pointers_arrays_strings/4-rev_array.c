2cf9d9620ebc7b3fea2eebafce666bc0ed8cc91a
Dennismwendwa <dennismusemb2@.com>
2023-03-01 12:21:23 +0300

    function that compares arrays

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * reverse_array - Function that reverses arrays.
+  * @a:- single user input.
+  * @n:- single user input.
+  */
+
+void reverse_array(int *a, int n)
+{
+	int tmp;
+	int i;
+
+	for (i = n-1; i >= n / 2; i--)
+	{
+		tmp = a[n - 1 - i];
+		a[n - 1 -i] = a[i];
+		a[i] = tmp;
+
+	}
+}
