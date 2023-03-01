8d6bd00634578f8fa9791410bb47337cef1eba8d
Dennismwendwa <dennismusemb2@.com>
2023-03-01 19:13:08 +0300

    updated file betty problem

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
-  * _strcat - Function that concentrates two strings.
+  * _strncat - Function that concentrates two strings.
   * @dest:- The first string.
   * @src:- The second string.
   * @n:- the number of bytes to be used from src.
-  * Return:- Always 0 (success). here we return the value of dest after joining.
+  * Return:- Always 0(success).here we return the value of dest after joining.
   */
 
 
