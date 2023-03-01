8c5376342415e8e57c2a35cf2ea86a6ef3db8239
Dennismwendwa <dennismusemb2@.com>
2023-03-01 20:35:06 +0300

    encoding strings to 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,22 +9,23 @@
 
 char *leet(char *s)
 {
-	int i;
+	int i = 0;
+	int j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (s[i])
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j <= 7; j++)
+		{
+			if (s[i] == leet[j] || s[i] - 32 == leet[j])
+			{
+				s[i] = j + '0';
 
+			}
+		}
+
+	i++;
 	}
 	return (s);
-
 }
