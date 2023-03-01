3d4a0927c31766e027cefc4934f8285725db51dc
Dennismwendwa <dennismusemb2@.com>
2023-03-01 16:50:15 +0300

    printing integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+  * print_number - Function that prints integer.
+  * @n:- single user input.
+  */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+
+	if ((i / 10) > 0)
+	{
+		print_number(i / 10);
+
+	}
+	_putchar((i % 10) + '0');
+
+}
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+ #include <unistd.h>
+ #include "main.h"
+ /**
+   * main - entry point
+   *
+ * Return:- Always 0 (success)
+     */
+ 
+ int _putchar(char c)
+{
+	 return (write(1, &c, 1));
+}
+
