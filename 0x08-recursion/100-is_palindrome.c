5ff11fea46b762158618604e3877861df95b7b49
Dennismwendwa <dennismusemb2@.com>
2023-03-08 16:01:26 +0300

    printing palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+
+/**
+  * palindromeString - checking iif string is palindrome.
+  * @len:- single user input.
+  * @i:- single user input.
+  * @len:- varible tracker.
+  * return:- Always 0 (success)
+  */
+
+int palindromeString(char *str, int len, int i)
+{
+	if (i < len && str[i] == str[len])
+	{
+		return (palindromeString(str, len - 1, i + 1));
+	}
+
+	if (str[i] != str[len])
+	{
+		return (0);
+	}
+
+	return (1);
+
+}
+
+/**
+  * _strlen_recursion - function that checks length of string.
+  * @s:- single user input.
+  * Return:- Always 0 (success)
+  */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+
+	return (0);
+}
+
+
+/**
+  * is_palindrome - Function to check if string is palindrome.
+  * @s:- single user input.
+  * Return:- always 0 (success).
+  */
+
+int is_palindrome(char *s)
+{
+	int k = 0;
+	int leng;
+
+	leng = _strlen_recursion(s) - 1;
+
+	return (palindromeString(s, leng, k));
+
+}
