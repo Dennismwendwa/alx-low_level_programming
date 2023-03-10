f9333591d21bc6971b58c3f281d1f9683dd1cd69
Dennismwendwa <dennismusemb2@.com>
2023-03-10 15:13:23 +0300

    printing all args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry point.
+  * @argc:- commandlines args.
+  * @argv:- commandline args.
+  * Return:- Always 0 (success)
+  */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+
+		argc = argc;
+
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+
+		}
+	return (0);
+
+}
