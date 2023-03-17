43f440bb2e7ab5165dc3bba0e9ab68040b85f432
Dennismwendwa <dennismusemb2@.com>
2023-03-17 04:23:03 +0300

    updated file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -65,47 +65,45 @@ void exit_err(void)
 
 int main(int argc, char *argv[])
 {
-	int len1, len2, len, i, *result;
-	int num1, num2, carr, x = 0;
 
 	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-	if (argc != 3|| !is_digit(s1) || !is_digit(s2))
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		exit_err();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (len1 = len2 - 1; len1 >= 0; len1--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		num1 = s1[len1] - '0';
-		carr = 0;
+		digit1 = s1[len1] - '0';
+		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			num2 = s2[len2] - '0';
-			carr += result[len1 + len2 + 1] + (num1 * num2);
-			result[len1 + len2] = carr % 10;
-			carr /= 10;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		if (carr > 0)
-			result[len1 + len2 + 1] += carr;
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-		for (i = 0; i < len - 1; i++)
-		{
-			if (result[i])
-				x = 1;
-			if (x)
-				_putchar(result[i] + '0');
-		}
-		if (!x)
-			_putchar('0');
-		_putchar('\n');
-		free(result);
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
