6d90ab7c92685eb4c3138fbc0f970fa43d8fa5df
Dennismwendwa <dennismusemb2@.com>
2023-03-20 13:14:04 +0300

    function that prints dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,10 +3,18 @@
 
 #include "dog.h"
 
+/**
+ * print_dog - function that prints dog
+ * @d:- single member
+ */
+
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d)
+	{
+		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
+		printf("Age: %f\n", d->age);
+		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
+	}
 
 }
