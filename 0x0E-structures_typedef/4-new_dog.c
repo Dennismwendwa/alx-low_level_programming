0f597ff2dd190c89b49bc0d178aae5169ad15c99
Dennismwendwa <dennismusemb2@.com>
2023-03-20 12:20:12 +0300

    creating new dog type

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dog.h"
+
+/**
+ *
+ * new_dog - function name.
+ * name:- first member
+ * @age:- second member
+ * @owner:- third member
+ * return:- Always 0 (success)
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i;
+	int len1;
+	int len2;
+	dog_t *ndog;
+
+	len1 = len2 = 0;
+
+	while (name[len1++])
+		;
+	while (owner[len2++])
+		;
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
+
+	ndog->name = malloc(len1 * sizeof(ndog->name));
+	if (ndog == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		ndog->name[i] = name[i];
+
+	ndog->age = age;
+
+	ndog->owner = malloc(len2 * sizeof(ndog->owner));
+	if (ndog == NULL)
+		return (NULL);
+	for (i = 0; i < len2; i++)
+		ndog->owner[i] = owner[i];
+
+	return (ndog);
+}
