1862082bee5bd4efabad6441fd0d7a1f2ed42ee0
Dennismwendwa <dennismusemb2@.com>
2023-03-22 17:01:11 +0300

    performs operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -21,7 +21,7 @@ int op_sub(int num1, int num2);
 int op_mul(int num1, int num2);
 int op_div(int num1, int num2);
 int op_mod(int num1, int num2);
-
+int (*get_op_func(char *s))(int, int);
 
 
 #endif
