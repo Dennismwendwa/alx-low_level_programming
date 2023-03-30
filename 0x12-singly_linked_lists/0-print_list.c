eed685b084e1908bd74918d03152a294f2cfdf89
Dennismwendwa <dennismusemb2@.com>
2023-03-30 10:47:19 +0300

    print all elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lists.h"
+
+/**
+  * size_t print_list - function that prints all elements of list
+  * @h:- single user input
+  * Return:- Always 0 (success)
+  */
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+	}
+	return (i);
+
+}
