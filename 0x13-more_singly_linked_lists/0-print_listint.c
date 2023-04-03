cdf2230bacb4d8fcd30079655b227a5bcd02e98d
Dennismwendwa <dennismusemb2@.com>
2023-04-03 11:02:39 +0300

    printing all elements in linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+
+/**
+  * print_listint - function that prints linked list
+  * @h:- head of the linked list
+  * Return:- Always 0 (success)
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+We are doing more singly linked lists
