e06b79d105edebd7c528be59b36712bb503eb10e
Dennismwendwa <dennismusemb2@.com>
2023-04-03 20:53:44 +0300

    deleting element

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -34,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			{
 				previous->next = current->next;
 				free(current);
-				current = NULL;
+
 				return (1);
 			}
 			previous = current;
