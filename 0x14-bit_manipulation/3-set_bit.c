8f82aa5568a850bf1879c1abfaba6ec521ae699b
Dennismwendwa <dennismusemb2@.com>
2023-04-06 17:09:28 +0300

    setting value of bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,28 @@
+
+#include "main.h"
+
+/**
+  * set_bit - function that set the value of bit to 1
+  * @index:- index of the bit
+  * @n:- single user input
+  * Return:- Always 0 (success)
+  */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	unsigned int size;
+	int l;
+
+
+	size = 8 * sizeof(*n) - 1;
+	l = 1 << index;
+
+
+	*n = *n | l;
+	return (1);
+
+
+	if (index > size)
+		return (-1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,7 +11,7 @@ int _strlen(char *s);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
-
+int set_bit(unsigned long int *n, unsigned int index);
 
 
 
