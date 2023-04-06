a1246f08919311c008fd84a0d7bce52111610680
Dennismwendwa <dennismusemb2@.com>
2023-04-06 16:52:00 +0300

    checking the bit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+
+#include "main.h"
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int k = 1 << 31;
+
+
+	
+	while (k > 0)
+	{
+
+	}
+}
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+
+#include "main.h"
+
+/**
+  * get_bit - function that returns number of bits
+  * @index:- index of the bit
+  * @n:- single user input
+  * Return:- Always 0 (success)
+  */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int size;
+	int l;
+	int bit;
+	
+	size = 8 * sizeof(n) - 1;
+	l = (int)(n >> index) & 1;
+
+	bit = (index > size) ? - 1 : l;
+
+	return (bit);
+}
