883cc18f7d2ac394a5fe51df53286dd7464c3b19
Dennismwendwa <dennismusemb2@.com>
2023-04-11 02:10:48 +0300

    decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,33 @@
 
 #include "main.h"
 
+/**
+  * print_binary - function that convets binary frm decimal
+  * @n:- number to convert
+  * Return:- Always 0 (success)
+  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int k = 1 << 31;
+	int k;
+	unsigned long int fflag = 0;
+	unsigned long int l;
+
+	k = sizeof(n) * 8 - 1;
 
 
-	
-	while (k > 0)
+	while (k >= 0)
 	{
+		l = n >> k;
 
+		if (l & 1)
+			fflag = 1;
+		if (fflag == 1)
+			(l & 1) ? _putchar('1') : _putchar('0');
+
+		k--;
 	}
+
+	if (n == 0)
+		_putchar('0');
 }
