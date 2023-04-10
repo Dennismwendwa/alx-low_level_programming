06285fa2bf59d370549c1279d6fb9a64a8afec29
Dennismwendwa <dennismusemb2@.com>
2023-04-11 00:00:12 +0300

    creating file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,29 +9,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int createdfile;
-	int size = 0;
-	int begg;
+	ssize_t createdfile;
+	ssize_t size = -1;
+	int len = strlen(text_content);
 
-	createdfile = open(filename, O_WRONLY | O_CREAT | S_IRUSR | S_IWUSR);
+	if (!filename)
+		return (-1);
+
+	createdfile = open(filename, O_CREAT | O_WRONLY |  O_TRUNC, 0600);
 	if (createdfile == -1)
 		return (-1);
 
-	for (; text_content[size]; size++)
-		;
-
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		close(createdfile);
-		return (1);
+		size = write(createdfile, text_content, len);
 	}
+	else
+		return (1);
 
-	begg = write(createdfile, text_content, size);
-	if (close(createdfile) == -1)
-		return (-1);
+	close (createdfile);
 
-	if (filename == NULL)
-		return (-1);
-
-	return (begg == -1 ? -1 : 1);
+	return ((size != -1) ? 1 : -1);
 }
