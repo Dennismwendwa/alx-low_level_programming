1741c6e50f638bc302a0a7e2ff5808acad875c63
Dennismwendwa <dennismusemb2@.com>
2023-04-11 12:16:55 +0300

    binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,26 +9,23 @@
   * Return:- Always 0 (success)
   */
 
-unsigned int _atoi(char c)
-{
-	return ((unsigned int) c - '0');
-}
-
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int number = 0;
 	unsigned int wegt = 1;
-	unsigned int tmp = 0;
 
 	int i = strlen(b) - 1;
 
-	for (; b[i]; wegt = wegt * 2, i--)
+	for (; i >= 0; i--)
 	{
-		tmp = _atoi(b[i]);
-		number += tmp * wegt;
-
 		if (b[i] != '1' && b[i] != '0')
 			return (0);
+
+		if (b[i] == '1')
+		{
+			number += wegt;
+		}
+		wegt *= 2;
 	}
 
 	if (b == NULL)
