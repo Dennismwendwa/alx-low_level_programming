6cbc8186b430d426dce0084ca20cc2ed61ba2824
Dennismwendwa <dennismusemb2@.com>
2023-04-14 21:35:52 +0300

    function that creates files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,31 @@
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t createdfile;
-	ssize_t size = -1;
-	int len = strlen(text_content);
+	ssize_t size;
+	int len;
 
 	if (!filename)
 		return (-1);
 
-	createdfile = open(filename, O_CREAT | O_WRONLY |  O_TRUNC, 0600);
+	if (text_content == NULL)
+	{
+		createdfile = open(filename, O_CREAT, 0600);
+		if (createdfile == -1)
+			return (-1);
+		close(createdfile);
+		return (1);
+	}
+	len = strlen(text_content);
+	createdfile = open(filename, O_TRUNC | O_RDWR | O_CREAT, 0600);
+
 	if (createdfile == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		size = write(createdfile, text_content, len);
-	}
-	else
-		return (1);
+	size = write(createdfile, text_content, len);
+	if (size == -1)
+		return (-1);
 
 	close(createdfile);
 
-	return ((size != -1) ? 1 : -1);
+	return (1);
 }
