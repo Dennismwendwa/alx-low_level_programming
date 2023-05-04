66ef89f4ccec1e24bb6842c9c9aa9e2b1ff7c72a
Dennismwendwa <dennismusemb2@.com>
2023-05-04 23:10:02 +0300

    convert binary to number

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,57 @@
+
+#include "main.h"
+
+size_t _strlen(const char *s);
+
+/**
+  * binary_to_uint - function that converts binary to digit
+  * @b:- pointer to the binary
+  * Return:- Always 0
+  */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int string_leng = _strlen(b);
+	int total_value = 0;
+	int base_val = 1;
+	int k;
+
+	if (!b)
+	{
+		return (0);
+	}
+
+	for (k = (string_leng - 1); k >= 0; k--)
+	{
+		if (b[k] != '0' && b[k] != '1')
+			return (0);
+	}
+
+	for (k = (string_leng - 1); k >= 0; k--)
+	{
+		if (b[k] == '1')
+		{
+			total_value += base_val;
+		}
+		base_val *= 2;
+	}
+	return (total_value);
+}
+
+/**
+  * _strlen - function that checks length of string
+  * @s:- pointer to the string
+  * Return:- Always 0
+  */
+
+size_t _strlen(const char *s)
+{
+	size_t length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
