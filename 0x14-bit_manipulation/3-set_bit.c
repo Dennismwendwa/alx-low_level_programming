73123eee96f9c1342667471fe44e388adde8ab82
Dennismwendwa <dennismusemb2@.com>
2023-05-05 18:18:49 +0300

    corrections for set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,20 +9,31 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int shujaa = 1;
+	unsigned int shujaa;
 	unsigned int size;
 
-	size = sizeof(size_t) * 8;
+	/*
+	 * size = sizeof(size_t) * 8; shujaa was 1,
+	 * shujaa <<= index;
+	 * *n |= shujaa;
+	 * if (index > size)
+	 * {
+	 * return (-1);
+	 * }
+	 * return (1);
+	 */
 
-
-	shujaa <<= index;
-
-	*n |= shujaa;
-
-	if (index > size)
-	{
+	if (n == 0)
 		return (-1);
-	}
+
+	size = 8 * sizeof(*n) - 1;
+	shujaa = 1 << index;
+
+	*n = *n | shujaa;
 
 	return (1);
+
+	if (index > size)
+		return (-1);
+
 }
