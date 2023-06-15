7f3e997cfc12dfea54e02b8da283f00a4742a1d9
Dennismwendwa <dennismusembi2@gmail.com>
2023-06-15 17:37:06 +0300

    print elements

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+* print_dlistint - function that prints elements of the list
+* @h:- the head of the list
+* Return:- Always 0
+*/
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
