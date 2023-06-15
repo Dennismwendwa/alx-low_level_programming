d629ace59d4eaaeb7b2e74deac407aef32e59c22
Dennismwendwa <dennismusembi2@gmail.com>
2023-06-15 21:25:47 +0300

    counting elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* dlistint_len - function that counts elements
+* @h:- head ptr
+* Return:- Always 0
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
