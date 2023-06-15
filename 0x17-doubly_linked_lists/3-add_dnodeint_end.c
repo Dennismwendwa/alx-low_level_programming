5dc1b717c609acf78ce50a1811c062b0a5ef407b
Dennismwendwa <dennismusembi2@gmail.com>
2023-06-16 00:00:51 +0300

    adding node end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+  * add_dnodeint_end - function that adds node at the end
+  * @head:- ptr to head
+  * @n:- position to add node
+  * Return:- Always 0
+  */
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *tmp;
