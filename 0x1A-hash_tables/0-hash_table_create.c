6072352b6c8e0e9d54fca3b225be26b698c32420
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-06 11:43:29 +0300

    hash table created

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that create hashtable
+ * and its head points to NULL
+ * @size:- the size of the hashtable
+ * Return:- Always 0
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable;
+	unsigned int j;
+
+	if (1 > size)
+	{
+		return NULL;
+	}
+
+	hashtable = malloc(sizeof(hash_table_t));
+	if (!hashtable)
+	{
+		return NULL;
+	}
+
+	hashtable->array = malloc(sizeof(hash_table_t *) * size);
+	if (!(hashtable->array))
+	{
+		return NULL;
+	}
+
+	for (j = 0; j < size; j++)
+	{
+		hashtable->array[j] = NULL;
+	}
+	hashtable->size = size;
+
+	return hashtable;
+}
